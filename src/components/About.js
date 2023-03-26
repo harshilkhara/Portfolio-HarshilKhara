4bb99d007345e18a8b9147ed576b8c1a646ae297
harshilkhara <kharaharshil@gmail.com>
2023-03-25 21:53:42 -0400

    Updated resume link

--- src/components/About.js
+++ src/components/About.js
@@ -44,7 +44,7 @@ class About extends Component {
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   
-                  <a  id ='linktag' href='https://drive.google.com/file/d/1MUifgmEdUjrm5VWpsNvR43qyiF2fD2nm/view?usp=sharing' target="_blank" rel="noopener noreferrer" className="card-body font-trebuchet text-justify ml-3 mr-3" style={{'font-size': '15px','margin-top':'100px'}}>R E S U M E</a>
+                  <a  id ='linktag' href='https://drive.google.com/file/d/1wKEoivDrrphaHGglLRmuThprFjwrxGGi/view?usp=sharing' target="_blank" rel="noopener noreferrer" className="card-body font-trebuchet text-justify ml-3 mr-3" style={{'font-size': '15px','margin-top':'100px'}}>R E S U M E</a>
                 </span>
               </div>
             </div>
